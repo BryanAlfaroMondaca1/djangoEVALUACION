4043ec76763a03146bf0537de98e6264f0b636f0
BryanAlfaroMondaca1 <167895521+BryanAlfaroMondaca1@users.noreply.github.com>
2024-09-11 10:33:25 -0300

    Initial commit

---
+++ evaluacion/productos/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'productos/index.html')
+def productos_view(request):
+    categorias = {
+'Frutas': ['Manzana', 'Banana', 'Pera'],
+'Verduras': ['Tomate', 'Lechuga', 'Zanahoria'],
+'Lácteos': ['Leche', 'Yogurt', 'Queso']
+}
+    return render(request, 'productos/productos.html', {'categorias': categorias})
+
